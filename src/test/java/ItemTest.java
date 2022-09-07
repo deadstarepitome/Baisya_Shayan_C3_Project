ce75c67b0fed26f0996a10b533daeb2999a983cb
deadstarepitome <shayan26@live.com>
2022-09-07 19:37:23 +0530

    Part3 : Failing Test case

---
+++ src/test/java/ItemTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+   public void getOrderTotal() {
+        assertEquals(450,460);
+    }
+}
